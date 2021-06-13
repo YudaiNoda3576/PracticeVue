20feb391bea15fe1db9412f42011952542125081
YudaiNoda <yudainoda@YudaiNodanoMacBook-Air.local>
2021-06-13 12:38:23 +0900

    v-ifのテスト

--- js/sample.js
+++ js/sample.js
@@ -1,6 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      toggle: true,
+      message: 'Hello Vue!',
+      count: 0,
+      user:{
+        lastName: "野田",
+        firstName: "雄大",
+        prefecture: "滋賀",
+        age: 28,
+      },
+      colors:["赤","緑","青"]
     }
   })
