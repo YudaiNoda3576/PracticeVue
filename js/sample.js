a4f0a976b56b362f7850eba9974ac5ca5de0358a
YudaiNoda <yudainoda@YudaiNodanoMacBook-Air.local>
2021-06-21 23:43:55 +0900

    基本的な構文の学習

--- js/sample.js
+++ js/sample.js
@@ -1,11 +1,25 @@
+Vue.filter('numberFormat', function(v){
+  return v.toLocaleString()
+})
+
+Vue.filter('jpyToUsd', function(jpyPrice){
+  return jpyPrice / 100
+})
+
+Vue.filter('readMore', function(longText, length, suffix){
+  return longText.substring(0, length) + suffix
+})
+
 Vue.component('hello-world-component', {
     template:'<p>Hello Worldのコンポーネントです</p>'
 });
+
 var app = new Vue({
     el: '#app',
     data: {
       toggle: false,
       message: 'Hello Vue!',
+      html: '<span style="color: red;">Hello Vue!</span>',
       count: 0,
       user:{
         lastName: "野田",
@@ -14,7 +28,12 @@ var app = new Vue({
         age: 28,
       },
       colors:["赤","緑","青"],
-      now: ""
+      now: "",
+      number: 100,
+      isOk: false,
+      jpyPrice: 2980000,
+      longText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      url:"https://en.wikipedia.org/wiki/Lorem_ipsum"
     },
     methods: {
       onclick: function(){
@@ -23,5 +42,10 @@ var app = new Vue({
       clickHandler: function(e){
         this.message = this.message.split('').reverse().join('')
       }
-    }
+    },
+    // filters: {
+    //   numberFormat: function(v){
+    //     return v.toLocaleString()
+    //   }
+    // }
 })
