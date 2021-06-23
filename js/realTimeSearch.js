f95da911b241ff4a04657d6e0e2b7534c8cc2d87
YudaiNoda <yudainoda@YudaiNodanoMacBook-Air.local>
2021-06-23 23:17:19 +0900

    QiitaApiを用いたアプリケーションの実装

--- js/realTimeSearch.js
+++ js/realTimeSearch.js
@@ -1,17 +1,43 @@
 let app = new Vue({
     el: "#app",
     data: {
-        items:null,
+        items: null,
         keyWord: '',
         message: ''
     },
     watch: {
-
+        keyWord: function(newVal, oldVal){
+            console.log(newVal)
+            this.message = "入力中｡｡｡"
+            this.debouncedGetAnswer()
+        }
     },
     created: function(){
-
+        //this.keyWord = "入力してください",
+        //this.getAnswer()
+        this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
     },
     methods: {
-
+        getAnswer: function(){
+            if(this.keyWord === ''){
+                this.items = null
+                this.message = ''
+                return
+            }
+            this.message = "Loading..."
+            let vm = this
+            let params = {page: 1, per_page:20, query:this.keyWord} 
+            axios.get("https://qiita.com/api/v2/items", { params })
+                .then(function(response){
+                    console.log(response)
+                    vm.items = response.data
+                })
+                .catch(function(error){
+                    vm.message = "エラーです:" + error
+                })
+                .finally(function(){
+                    vm.message = ""
+                })
+        }
     }
 });
